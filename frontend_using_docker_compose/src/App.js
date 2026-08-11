01052f0461c8a40336dbd0f173266db047143e93
Yadanar016 <yadanarhtun.eduvalor.2024igcse@gmail.com>
2026-08-11 14:30:04 +0800

    Add frontend_implementing_multisteps, multisteps_Prod, and all untracked files

--- frontend_using_docker_compose/src/App.js
+++ frontend_using_docker_compose/src/App.js
@@ -9,22 +9,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Containerized Web Application Dashboard</h1>
+        <h1>Welcome to React</h1>
         
         <div className="card">
           <p><strong>Developed by:</strong> Yadanar and Jolyn</p>
           <p><strong>Environment:</strong> Docker Development Setup</p>
           
           <button className="custom-btn" onClick={() => setLikes(likes + 1)}>
-            👍 Click to Test Interactivity: {likes}
+             Click to Test Interactivity: {likes}
           </button>
         </div>
 
-        <div className="badges">
-          <span className="badge">Docker</span>
-          <span className="badge">React</span>
-          <span className="badge">Nginx</span>
-        </div>
+        
       </header>
     </div>
   );
