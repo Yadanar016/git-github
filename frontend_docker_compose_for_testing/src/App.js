17ce2e7d157a19a332bb04fe363a12bb21bf3ff2
Yadanar016 <yadanarhtun.eduvalor.2024igcse@gmail.com>
2026-08-11 14:54:43 +0800

    frontend_implementing-muitisteps

--- frontend_docker_compose_for_testing/src/App.js
+++ frontend_docker_compose_for_testing/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to React App</h1>
+        <h1>Welcome to React App Testing</h1>
         
         <div className="card">
           <p><strong>Developed by:</strong> Yadanar and Jolyn</p>
